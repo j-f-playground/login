f4b72e381961d8a6968ffc75c7aad7fbb8263438
Estefanía Salgado Alameda <estefania.salgado@gmail.com>
2019-08-21 11:12:11 +0200

    img component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,22 +2,29 @@ import React, { Component } from 'react';
 import Text from 'components/Text/';
 import Input from 'components/Input/';
 import Button from 'components/Button/';
+import Image from 'components/Image/';
 import styles from './styles.module.scss';
+import smile from './img/smile.png';
+
 
 class Login extends Component {
   render() {
     return (
       <div className={styles.container}>
         <div className={styles.loginContainer}>
+          <Image source={smile} imgStyle={styles.image} alt='Smile'/>
           <div className={styles.login}>
-            <Text text={'Username'} textType='grey' />
+            <Text text='Username' textType='grey' />
             <Input />
-            <Text text={'PassWord'} textType='grey' />
+            <Text text='PassWord' textType='grey' />
             <Input />
-            <Button text={'Sign In'} type='green' textType='white-' />
+            <Button text='Sign In' type='green' textType='white' />
             </div>
             <div className={styles.forgotPass}>
-              <Button text={'Forgot Password?'} type='white' textType='grey' />
+              <Button text='Forgot Password?' type='white' textType='grey' />
+            </div>
+            <div className={styles.newAccountText}>
+              <Text text='Don’t have an account yet? Sign Up' textType='white' />
             </div>
         </div>
       </div>
