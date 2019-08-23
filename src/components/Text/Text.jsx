19509310d34e9a3ac1717db2183974152b3fbddf
Estefanía Salgado Alameda <estefania.salgado@gmail.com>
2019-08-23 15:29:32 +0200

    width login

--- src/components/Text/Text.jsx
+++ src/components/Text/Text.jsx
@@ -6,16 +6,18 @@ class Text extends Component {
   static propTypes = {
     text: string.isRequired,
     textType: string,
+    className: string,
   };
 
   static defaultProps = {
     textType: undefined,
+    textStyle: undefined,
   };
 
   render() {
-    const { text, textType } = this.props;
+    const { text, textType, className } = this.props;
     return (
-      <div className={`${styles.text} ${textType === 'grey' ? styles.grey : styles.white}`}
+      <div className={`${styles.text} ${className} ${textType === 'grey' ? styles.grey : styles.white}`}
 >
         {text}
       </div>
