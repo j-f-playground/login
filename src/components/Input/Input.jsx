05304b33bfd4bbf29b965150b33b69c03e73ac2b
Estefanía Salgado Alameda <estefania.salgado@gmail.com>
2019-08-26 11:41:14 +0200

    error inputs

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
-import { bool } from 'prop-types';
+import { bool, func, string } from 'prop-types';
 import styles from './styles.module.scss';
 
 class Input extends Component {
   static propTypes = {
     error: bool,
+    onChange: func.isRequired,
+    name: string.isRequired,
   };
 
 static defaultProps = {
-  error: undefined,
+  error: false,
 };
+
+handleOnChange = (event) => {
+  const value = event.currentTarget.value;
+  this.props.onChange(this.props.name, value);
+}
 
   render() {
     const { error } = this.props;
     return (
-      <input className={`${styles.input} ${error ? styles.inputError : ''}`} />
+      <input className={`${styles.input} ${error
+         ? styles.inputError : ''}`} onChange={this.handleOnChange} />
     );
   }
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -8,18 +8,76 @@ import smile from './img/smile.png';
 
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      inputOk: true,
+      ifErrorEmail: false,
+      ifErrorPass: false,
+    };
+  };
+
+  handleOnchange = (name, value) => {
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  sendData = () => {
+    let url = '';
+    if (this.state.email === 'logintest@jfplayground.com' && this.state.password === 'test') {
+      url = 'https://httpstat.us/200';
+    } else {
+      url = 'https://httpstat.us/503';
+      if (this.state.email !== 'logintest@jfplayground.com') {
+        this.setState({
+          ifErrorEmail: true,
+        })
+      } if (this.state.password !== 'test') {
+        this.setState({
+          ifErrorPass: true,
+        })
+      }
+    }
+
+    fetch(url)
+    .then(() => (
+      this.setState({
+        inputOk: false,
+      })
+    ))
+    .catch(() => (
+      this.setState({
+        inputOk: true,
+      })
+    ));
+  };
+
   render() {
+    console.log(this.state);
+    const { inputOk, ifErrorPass, ifErrorEmail } = this.state;
     return (
       <div className={styles.container}>
         <div className={styles.loginContainer}>
           <Image source={smile} imgStyle={styles.image} alt='Smile'/>
-          <div className={styles.login}>
+          {inputOk ?
+            <div className={styles.login}>
             <Text text='Username' textType='grey' />
-            <Input />
+            <Input onChange={this.handleOnchange} name='email' error={ifErrorEmail} />
             <Text text='PassWord' textType='grey' />
-            <Input />
-            <Button text='Sign In' type='green' textType='white' styleButton={styles.singInButton} />
+            <Input onChange={this.handleOnchange} name='password' error={ifErrorPass} />
+            <Button
+              text='Sign In'
+              type='green'
+              textType='white'
+              styleButton={styles.singInButton}
+              onClick={this.sendData}
+            />
           </div>
+          : 
+          <div>OK!</div>}
           <div className={styles.forgotPass}>
             <Button text='Forgot Password?' type='white' textType='grey'/>
           </div>
